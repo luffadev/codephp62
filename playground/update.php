de3cba4549ec9c774fedbe905fbc5c2c0a0052ab
Suradet Petcharanon <petcharanon@gmail.com>
2021-04-07 16:51:17 +0700

    add bootstrap

--- playground/update.php
+++ playground/update.php
@@ -1,4 +1,5 @@
 <?php
+include 'template/header.html';
 require_once 'connectdb.php';
 
 $id = "";
@@ -21,14 +22,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $status = $_POST["status"];
     // echo $username . " -- " .$status;
-    $strSQL = "UPDATE users SET username='" .$username ."',status=".$status." WHERE id=".$id;
+    $strSQL = "UPDATE users SET username='" . $username . "',status=" . $status . " WHERE id=" . $id;
     if (($username == "") && ($status == "")) {
         echo "ไม่สามารถเพิ่มข้อมูลได้";
-    }else{
-        echo $strSQL;
+    } else {
         $result = $myConn->query($strSQL);
         if ($result) {
-            echo "เพิ่มข้อมูลสำเร็จ";
+?>
+            <a href="select.php">กลับหน้ารายชื่อ</a>
+<?php
         } else {
             echo "ไม่สามารถเพิ่มข้อมูลได้";
         }
@@ -37,32 +39,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
 
 <body>
-    <form action="update.php?id=<?=$id?>" method="POST">
-        <table border="1">
-            <tr>
-                <td>Username</td>
-                <td><input type="text" name="username" value="<?=$username?>" ></td>
-            </tr>
-            <tr>
-                <td>Status</td>
-                <td><input type="text" name="status" value="<?=$status?>"></td>
-            </tr>
-            <tr>
-                <td colspan="2"><input type="submit" value="save"></td>
-            </tr>
-        </table>
+    <form action="update.php?id=<?=$id?>" method="post">
+        <div class="form-group">
+            <label for="exampleInputEmail1">Username</label>
+            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="username" value="<?= $username ?>">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Status</label>
+            <input type="text" class="form-control" id="exampleInputPassword1" name="status" value="<?= $status ?>">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Save</button>
     </form>
+    <?php
+    include 'template/footer.html';
+    ?>
 </body>
 
 </html>
