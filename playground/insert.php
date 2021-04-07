e99d2c73445612021f75f06b30a70f31dd7a6efb
Suradet Petcharanon <petcharanon@gmail.com>
2021-04-07 16:44:26 +0700

    add css

--- playground/insert.php
+++ playground/insert.php
@@ -1,4 +1,5 @@
 <?php
+include 'template/header.html';
 require_once 'connectdb.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -10,7 +11,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $strSQL .= "VALUES ('" . $frmUsername . "','" . $frmPassword . "',0)";
         $result = $myConn->query($strSQL);
         if ($result) {
-            echo "เพิ่มข้อมูลสำเร็จ";
+?>
+            <a href="select.php">กลับหน้ารายชื่อ</a>
+<?php
         } else {
             echo "ไม่สามารถเพิ่มข้อมูลได้";
         }
@@ -19,33 +22,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
-    <title>Document</title>
-</head>
 
 <body>
     <form action="insert.php" method="post">
-        <table border="1">
-            <tr>
-                <td>username</td>
-                <td><input type="text" name="username"></td>
-            </tr>
-            <tr>
-                <td>password</td>
-                <td><input type="password" name="password"></td>
-            </tr>
-            <tr>
-                <td colspan="2"><input type="submit" value="save"></td>
-            </tr>
-        </table>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="username">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" name="password">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Save</button>
     </form>
+    <?php
+    include 'template/footer.html';
+    ?>
 </body>
 
 </html>
--- playground/select.php
+++ playground/select.php
@@ -7,28 +7,38 @@ $mysqli_statement = $myConn->query($statement);
 ?>
 
 <body>
-    <table style="width: 100%;" border="1">
-        <tr>
-            <td>รหัส</td>
-            <td>ชื้อผู้ใช้</td>
-            <td>สถานะ</td>
-            <td>แก้ไข</td>
-            <td>ลบ</td>
-        </tr>
-        <?php
-        while ($row = $mysqli_statement->fetch_array()) {
-        ?>
-        <tr>
-            <td><?php echo $row["id"]?></td>
-            <td><?php echo $row["username"]?></td>
-            <td><?php echo $row["status"]?></td>
-            <td><a href="update.php?id=<?= $row["id"]?>&username=<?= $row["username"]?>&status=<?= $row["status"]?>"><i class="far fa-edit"></i></a></td>
-            <td><a href="delete.php?id=<?= $row["id"]?>"><i class="far fa-trash-alt"></i></a></td>
-        </tr>
-        <?php
-        }
-        ?>
+    <table class="table">
+        <thead class="thead-dark">
+            <tr>
+                <th>รหัส</th>
+                <th>ชื้อผู้ใช้</th>
+                <th>สถานะ</th>
+                <th>แก้ไข</th>
+                <th>ลบ</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            while ($row = $mysqli_statement->fetch_array()) {
+            ?>
+                <tr>
+                    <td><?php echo $row["id"] ?></td>
+                    <td><?php echo $row["username"] ?></td>
+                    <td><?php echo $row["status"] ?></td>
+                    <td><a href="update.php?id=<?= $row["id"] ?>&username=<?= $row["username"] ?>&status=<?= $row["status"] ?>"><i class="far fa-edit"></i></a></td>
+                    <td><a href="delete.php?id=<?= $row["id"] ?>"><i class="far fa-trash-alt"></i></a></td>
+                </tr>
+            <?php
+            }
+            ?>
+        </tbody>
     </table>
+    <a href="insert.php">เพิ่มผู้ใช้</a>
+
+
+    <?php
+    include 'template/footer.html';
+    ?>
 </body>
 
 </html>
